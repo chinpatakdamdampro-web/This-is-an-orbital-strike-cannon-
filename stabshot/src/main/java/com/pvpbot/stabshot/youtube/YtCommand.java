a2875f4981155e6c48ff722f539834e754ea4088
Sigma47 <chinpatakdamdampro@gmail.com>
2026-07-09 14:06:18 +0530

    Update YtCommand.java

--- stabshot/src/main/java/com/pvpbot/stabshot/youtube/YtCommand.java
+++ stabshot/src/main/java/com/pvpbot/stabshot/youtube/YtCommand.java
@@ -4,6 +4,8 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.pvpbot.stabshot.youtube.YtSearch;
+import com.pvpbot.stabshot.youtube.YtPlayer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
