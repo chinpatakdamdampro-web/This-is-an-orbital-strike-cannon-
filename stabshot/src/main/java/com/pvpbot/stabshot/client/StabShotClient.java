7e3147282bd939e58b1e2960ae7083a0394f682c
Sigma47 <chinpatakdamdampro@gmail.com>
2026-07-09 13:45:19 +0530

    Update StabShotClient.java

--- stabshot/src/main/java/com/pvpbot/stabshot/client/StabShotClient.java
+++ stabshot/src/main/java/com/pvpbot/stabshot/client/StabShotClient.java
@@ -1,6 +1,5 @@
 package com.pvpbot.stabshot.client;
 
-import com.pvpbot.stabshot.spotify.SpotifyWebServer;
 import com.pvpbot.stabshot.themesong.ThemeSongCommand;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -12,11 +11,5 @@ public class StabShotClient implements ClientModInitializer
     @Override
     public void onInitializeClient() {
         ThemeSongCommand.register();
-
-        try {
-            SpotifyWebServer.getInstance().start();
-        } catch (Exception e) {
-            System.err.println("[StabShot] Could not start Spotify web server: " + e.getMessage());
-        }
     }
 }
